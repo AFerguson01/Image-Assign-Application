6fcba52ad42c2120d3d538daa372720144b54991
Adam Ferguson <61731043+AFerguson01@users.noreply.github.com>
2020-03-25 13:39:55 +0000

    Added email system and linked it to the sidenav. Added slick file for later implimentation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 var initialized = false;
 
+const emails = {
+};
+
 function SearchPhotos(){
     let clientId = "g_EmLyty4c1G5wxRV0-hURnJ6SI7B5GjoiaaWWBtT00"
     let query = document.getElementById("search").value;
@@ -10,6 +13,7 @@ function SearchPhotos(){
         assignBtn.id = "assign-btn";
         initialized = true;
     };
+    $('#assign-btn').prop("disabled", false);
 
     document.getElementById("search").value = "";
 
@@ -18,7 +22,44 @@ function SearchPhotos(){
             return data.json();
         })
         .then(function(data){
-            let result = `<img src="${data.results[0].urls.regular}" alt="${query} img">`;
+            let result = `<img src="${data.results[0].urls.regular}" alt="${query} img" id="image">`;
             $("#result").html(result);
         })
 }
+
+
+// Add email to emails object or if email already exists just add image.
+function AssignEmail(){
+    const email = document.getElementById("assign").value;
+    const img = document.getElementById("image");
+    if (emails.hasOwnProperty(`${email}`)){
+        emails[`${email}`].img += img;
+    }else{
+        emails[`${email}`] = {email: email, imgs: [img]};
+        UpdateSideNav(email);
+    }
+}
+
+
+// Create an email and append it to the sidenav
+function UpdateSideNav(email){
+    let listItem = document.createElement("LI");
+    listItem.classList.add("pushy-link")
+    listItem.classList.add("sidenav-link")
+    listItem.innerHTML = `<a>${emails[`${email}`].email}</a>`;
+    listItem.addEventListener("click", SelectEmail);
+    const ul = document.getElementById("sidenav");
+    ul.appendChild(listItem);
+}
+
+// Set clicked link to active and remove active class from other links
+function SelectEmail(e){
+    const link = e.target;
+    const links = (document.querySelectorAll(".sidenav-link a"));
+    for (let i=0; i < links.length;i++){
+        links[i].classList.remove("active");
+    }
+    link.classList.add("active");
+}
+
+
